50970df2ca3efa03b283ce25eb8802a51c3deacc
Toan Nguyen <toan310303@gmail.com>
2024-05-03 18:02:30 +0700

    Create all Migrations, Models and Seeders

--- src/migrations/migration-create-user.js
+++ src/migrations/migration-create-user.js
@@ -3,11 +3,14 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
-      id: {
+      user_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
       },
       firstName: {
         type: Sequelize.STRING
@@ -15,9 +18,25 @@ module.exports = {
       lastName: {
         type: Sequelize.STRING
       },
-      email: {
+      password: {
         type: Sequelize.STRING
       },
+      address: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.BOOLEAN
+      },
+      phonenumber: {
+        type: Sequelize.STRING
+      },
+      typeRole: {
+        type: Sequelize.STRING
+      },
+      keyRole: {
+        type: Sequelize.STRING
+      },
+
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- src/seeders/seeders-users.js
+++ src/seeders/seeders-users.js
@@ -2,9 +2,15 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [
       {
-        firstName: 'John',
-        lastName: 'Doe',
-        email: 'example@example.com',
+        email: 'admin@gmail.com',
+        firstName: 'Admin',
+        lastName: 'Admin',
+        password: 'admin',
+        address: 'IU',
+        phonenumber: '0123456789',
+        gender: 1,
+        typeRole: 'ROLE',
+        keyRole: 'R1',
         createdAt: new Date(),
         updatedAt: new Date(),
       },
