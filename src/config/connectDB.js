0d614625d2d3e5b9246971dbdb0f3af063f57fd3
PrettyUnnie1 <mrbin12192003@gmail.com>
2024-05-29 23:44:10 +0700

    setting database

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize('hospital', 'root', '123456', {
+const sequelize = new Sequelize('hospital', 'root', '19122003', {
   host: 'localhost',
   dialect: 'mysql',
   logging: false
