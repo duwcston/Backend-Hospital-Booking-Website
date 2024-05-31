9e308e6525c1b64b79b447c244b496d5a697f16e
PrettyUnnie1 <124762589+PrettyUnnie1@users.noreply.github.com>
2024-05-31 15:56:31 +0700

    76 fix bug

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -162,16 +162,16 @@ let bulkCreateSchedule = (data) => {
                 );
 
                 //convert date
-                if (existing && existing.length>0){
-                    existing = existing.map( item => {
-                        item.date = new Date(item.date).getTime();
-                        return item;
-                    })
-                }
+                // if (existing && existing.length>0){
+                //     existing = existing.map( item => {
+                //         item.date = new Date(item.date).getTime();
+                //         return item;
+                //     })
+                // }
 
                 //compare different
                 let toCreate = _.differenceWith(schedule, existing, (a,b) =>{
-                    return a.timeType === b.timeType && a.date === b.date;
+                    return a.timeType === b.timeType && +a.date === +b.date;
                 });
 
                 //create data
@@ -203,7 +203,13 @@ let getScheduleByDate = (doctorId, date) => {
                     where:{
                         doctorId: doctorId,
                         date: date
-                    }
+                    },
+
+                    include:[
+                        {model: db.Allcode, as:'timeTypeData', attributes:['valueEn','valueVi'] },
+                    ],
+                    raw: false,
+                    nest: true
                 })
 
                 if (!dataSchedule) dataSchedule = [];
