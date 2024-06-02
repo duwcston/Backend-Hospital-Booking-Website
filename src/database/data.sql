334ca3c206d6b1eba91ee9f63f90c66b1cfc124d
PrettyUnnie1 <mrbin12192003@gmail.com>
2024-06-03 02:27:18 +0700

    add more database

--- src/database/data.sql
+++ src/database/data.sql
@@ -1,4 +1,3 @@
-
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('ROLE','R1','Admin', 'Quản trị viên' ); 
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('ROLE','R2','Doctor', 'Bác sĩ' ); 
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('ROLE','R3','Patient', 'Bệnh nhân' ); 
@@ -8,10 +7,10 @@
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('STATUS','S3','Done', 'Đã khám xong' ); 
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('STATUS','S4','Cancel', 'Đã hủy' ); 
 
--- INSERT IGNORE  INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T1','8:00 AM - 9:00 AM', '8:00 - 9:00' ); 
--- INSERT IGNORE  INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T2','9:00 AM - 10:00 AM', '9:00 - 10:00' ); 
--- INSERT IGNORE  INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T3','10:00 AM - 11:00 AM', '10:00 - 11:00' ); 
--- INSERT IGNORE  INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T4','11:00 AM - 0:00 PM', '11:00 - 12:00' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T1','8:00 AM - 9:00 AM', '8:00 - 9:00' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T2','9:00 AM - 10:00 AM', '9:00 - 10:00' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T3','10:00 AM - 11:00 AM', '10:00 - 11:00' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T4','11:00 AM - 0:00 PM', '11:00 - 12:00' ); 
 
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T5','1:00 PM - 2:00 PM', '13:00 - 14:00' ); 
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('TIME','T6','2:00 PM - 3:00 PM', '14:00 - 15:00' ); 
@@ -27,3 +26,26 @@
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('GENDER','M','Male', 'Nam' ); 
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('GENDER','F','Female', 'Nữ' ); 
 -- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('GENDER','O','Other', 'Khác' ); 
+
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PRICE','PRI1','10', '200000' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PRICE','PRI2','15', '250000' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PRICE','PRI3','20', '300000' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PRICE','PRI4','25', '350000' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PRICE','PRI5','30', '400000' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PRICE','PRI6','35', '450000' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PRICE','PRI7','40', '500000' ); 
+
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PAYMENT','PAY1','Cash', 'Tiền mặt' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PAYMENT','PAY2','Credit card', 'Thẻ ATM' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PAYMENT','PAY3','All payment method', 'Tất cả' ); 
+
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO1','Ha Noi', 'Hà Nội' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO2','Ho Chi Minh', 'Hồ Chí Minh' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO3','Da Nang', 'Đà Nẵng' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO4','Can Tho', 'Cần Thơ' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO5','Binh Duong', 'Bình Dương' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO6','Dong Nai', 'Đồng Nai' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO7','Quang Ninh', 'Quảng Ninh' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO8','Hue', 'Thừa Thiên Huế' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO9','Quang Binh', 'Quảng Bình' ); 
+-- INSERT IGNORE INTO ALLCODES (ALLCODES.type,ALLCODES.keyMap, ALLCODES.valueEn, ALLCODES.valueVi) values ('PROVINCE','PRO10','Khanh Hoa', 'Khánh Hòa' ); 
